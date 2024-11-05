96db7b01cb526f5b6705cb99fae218762ee6208e
Daymaanku12 <daymaanku4@gmail.com>
2024-11-06 00:11:20 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', '__-Owner6x');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'owner6x');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '952283596');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'Checkerbotrobot');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', '@Checkerbotrobot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '8196793783:AAFEAW9gPPRQ23vErpDSPHCXe2z3kBnyn2E');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-4589892113');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002338620689');
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'ankit1221');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'ankit1221');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'ankit1221');
